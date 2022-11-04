9f6b6d19fa19e1e987bf3cb9b9ce93e43c1ed5d3
puneet2121 <bedipuneet21@gmail.com>
2022-11-04 04:08:56 -0600

    condtional reder

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.set('view engine', 'ejs')
+let items = [];
+
+app.use(bodyParser.urlencoded({extended:true}));
+let today = new Date();
+
+let options = {
+  day:'numeric',
+  month:'long',
+  year: 'numeric'
+}
+let date = today.toLocaleDateString('en-us',options);
+app.get('/',(req,res) => {
+   
+  res.render('lists',{theDay: date, tango:items })
+  
+});
+
+app.post('/',(req,res) => {
+  let item = req.body.t
+  items.push(item)
+  res.redirect('/')
+})
+app.listen(3000,() => {
+  console.log('server listening on 3000')
+})
