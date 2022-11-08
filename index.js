ed3b72a48c9dbf7f679ca8fc8719df77a3b5546a
puneet2121 <bedipuneet21@gmail.com>
2022-11-08 16:57:09 -0700

    fix db

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 dotenv.config();
-mongoose.connect(process.env.DATABASE_URL ,{useNewUrlParser: true, useUnifiedTopology: true});
-
+mongoose.connect(process.env.MONGO_URI ,{useNewUrlParser: true, useUnifiedTopology: true});
 const app = express();
 app.set("view engine", "ejs");
 
