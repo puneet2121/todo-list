2694ff656d69fcdf9682a2547bd93d10147d30a3
puneet2121 <bedipuneet21@gmail.com>
2022-11-08 22:07:11 -0700

    using asyn for db

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 dotenv.config();
-mongoose.connect(process.env.MONGO_URI ,{useNewUrlParser: true, useUnifiedTopology: true});
+const URL = process.env.MONGO_URI.toString()
+const connectDB =  async ()=>{
+
+  try{
+      const conn = await mongoose.connect(process.env.MONGO_URI,{
+          //must add in order to not get any error masseges:
+          useUnifiedTopology:true,
+          useNewUrlParser: true,
+      })
+      console.log(`mongo database is connected!!! ${conn.connection.host} `)
+  }catch(error){
+      console.error(`Error: ${error} `)
+      process.exit(1) //passing 1 - will exit the proccess with error
+  }
+
+}
+connectDB()
 const app = express();
 app.set("view engine", "ejs");
 
