cdad8476bea4f7d73c6dab81efec673d7d0e135a
puneet2121 <bedipuneet21@gmail.com>
2022-11-08 21:49:35 -0700

    put back url in env

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 dotenv.config();
-
-mongoose.connect( `mongodb+srv://admin-puneet:%40${process.env.PASSWORD}@cluster0.yyi9mqw.mongodb.net/todolistDB`,{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI ,{useNewUrlParser: true, useUnifiedTopology: true});
 const app = express();
 app.set("view engine", "ejs");
 
